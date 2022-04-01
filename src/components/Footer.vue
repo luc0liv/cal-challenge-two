1cbf48f463cd021270e55d0d6398bcc85f1e67b4
luc0liv <luoliveiradacunha@gmail.com>
2022-04-01 18:11:52 -0300

    Footer: criação do componente

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="footer-container">
-    <p>oie</p>
-  </div>
+  <section class="footer-container">
+    <div class="social-wrapper">
+      <p class="social-title">Follow us</p>
+      <div class="social-icons">
+        <a
+          v-for="(icon, index) in socialIcons"
+          :key="index"
+          :href="icon.url"
+          target="_blank"
+          rel="noopener noreferrer"
+          ><img
+            class="social-icon"
+            :src="require(`../assets/icons/${icon.iconPath}`)"
+            alt=""
+        /></a>
+      </div>
+    </div>
+    <div class="contact-wrapper">
+      <p class="contact-title">Contact</p>
+      <p class="contact-address">
+        2490 Leisure Lane San Luis Obispo California
+      </p>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -10,11 +31,15 @@ import { Options, Vue } from "vue-class-component";
 @Options({
   components: {},
 })
-export default class Footer extends Vue {}
+export default class Footer extends Vue {
+  socialIcons = [
+    { iconPath: "youtube-icon.svg", url: "https://www.youtube.com/" },
+    { iconPath: "facebook-icon.svg", url: "https://www.facebook.com/" },
+    { iconPath: "twitter-icon.svg", url: "https://www.twitter.com/" },
+  ];
+}
 </script>
 
-<style lang="scss" scoped>
-.footer-container {
-  background-color: #e9e9e9;
-}
+<style lang="scss">
+@import "@/assets/scss/footer.scss";
 </style>
