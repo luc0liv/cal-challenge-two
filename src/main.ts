ed4d6f0f41b3b8da5a886c04daf0f34f5b4291e6
luc0liv <luoliveiradacunha@gmail.com>
2022-04-01 18:10:36 -0300

    Criando o arquivo globals e as variáveis de cor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "./assets/scss/main.scss";
+import "./assets/scss/globals.scss";
 
 createApp(App).use(store).use(router).mount("#app");
