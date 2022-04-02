b411fcb49738880e0e7ca11559654fcbcbf48946
luc0liv <luoliveiradacunha@gmail.com>
2022-04-01 23:22:49 -0300

    Home: implementando navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
+    <Navbar></Navbar>
     <main-content></main-content>
     <Footer></Footer>
   </div>
 </template>
 
 <script lang="ts">
-import Footer from "@/components/Footer.vue";
-import MainContent from "@/components/MainContent.vue";
 import { Options, Vue } from "vue-class-component";
+import Navbar from "@/components/Navbar.vue";
+import MainContent from "@/components/MainContent.vue";
+import Footer from "@/components/Footer.vue";
 
 @Options({
-  components: { "main-content": MainContent, Footer: Footer },
+  components: { "main-content": MainContent, Footer: Footer, Navbar: Navbar },
 })
 export default class HomeView extends Vue {}
 </script>
