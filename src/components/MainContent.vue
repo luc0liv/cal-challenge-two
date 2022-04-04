05659d7791275524d8495fe17944b3132c8629d2
luc0liv <luoliveiradacunha@gmail.com>
2022-04-04 01:19:00 -0300

    Ajustes

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="section-one-container">
-    <p class="section-one-title">Measuring Everything</p>
-    <img
-      src="../assets/images/tapes-1.png"
-      alt="Fita métrica"
-      class="section-one-image"
-    />
-  </div>
-  <div class="section-two-container">
-    <div class="text-container">
-      <p class="section-two-title">We are Leader in Measure Tapes</p>
-      <span class="section-two-description"
-        >There are 5x the circumference of planet earth in metric tapes.</span
-      >
+  <main>
+    <div class="section-one-container">
+      <p class="section-one-title">Measuring Everything</p>
+      <img
+        src="../assets/images/tapes-1.png"
+        alt="Fita métrica"
+        class="section-one-image"
+      />
     </div>
-    <img
-      src="../assets/images/tape-2.png"
-      alt="Fita métrica"
-      class="section-two-image"
-    />
-  </div>
+    <div class="section-two-container">
+      <div class="text-container">
+        <p class="section-two-title">We are Leader in Measure Tapes</p>
+        <span class="section-two-description"
+          >There are 5x the circumference of planet earth in metric tapes.</span
+        >
+      </div>
+      <img
+        src="../assets/images/tape-2.png"
+        alt="Fita métrica"
+        class="section-two-image"
+      />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
